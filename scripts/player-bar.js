b5b6894afe0f489beb17a04126acd979e284ab88
chels62789 <chelseamaried.art@gmail.com>
2020-02-10 10:30:20 -0800

    submit 2

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -20,10 +20,10 @@ $('button#previous').on('click', function() {
   if (player.playState !== 'playing') { return; }
 
 const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-const nextSongIndex = currentSongIndex - 1;
-if (nextSongIndex >= album.songs.length) { return; }
-const nextSong = album.songs[nextSongIndex];
-player.playPause(nextSong);
+const previousSongIndex = currentSongIndex - 1;
+if (currentSongIndex - 1 < 0) { return; }
+const previousSong = album.songs[previousSongIndex];
+player.playPause(previousSong);
 });
 
 
